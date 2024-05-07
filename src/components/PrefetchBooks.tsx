b8642a27964193fc294626130a875026ade74e8e
Adam Rackis <adamrackis@hotmail.com>
2024-05-06 20:07:51 -0500

    Add as attribute

--- src/components/PrefetchBooks.tsx
+++ src/components/PrefetchBooks.tsx
@@ -3,8 +3,8 @@ import type { FC } from "react";
 export const PrefetchBooks: FC<{}> = (props) => {
   return (
     <>
-      <link rel="prefetch" href="/api/books" />
-      <link rel="prefetch" href="/api/books-count" />
+      <link rel="prefetch" href="/api/books" as="fetch" />
+      <link rel="prefetch" href="/api/books-count" as="fetch" />
     </>
   );
 };
